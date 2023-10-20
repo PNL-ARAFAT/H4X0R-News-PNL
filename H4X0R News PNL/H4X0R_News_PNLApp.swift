1921e901d001ff4878d352d4a464f2a6e437c73a
arafat2183 <akash076987@gmail.com>
2023-10-20 17:45:59 +0900

    Initial Commit

---
+++ H4X0R News PNL/H4X0R_News_PNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_PNLApp.swift
+//  H4X0R News PNL
+//
+//  Created by Akash Arafat on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_PNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
